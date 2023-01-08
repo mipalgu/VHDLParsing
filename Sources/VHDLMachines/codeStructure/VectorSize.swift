37a22ceeaeab5a2272b7df202a0849ee67b34fba
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-01-09 01:01:17 +1000

    Created a VectorSize enum.

---
+++ Sources/VHDLMachines/codeStructure/VectorSize.swift
@@ -0,0 +1,109 @@
+// VectorSize.swift
+// Machines
+// 
+// Created by Morgan McColl.
+// Copyright © 2023 Morgan McColl. All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+// 
+// 3. All advertising materials mentioning features or use of this
+//    software must display the following acknowledgement:
+// 
+//    This product includes software developed by Morgan McColl.
+// 
+// 4. Neither the name of the author nor the names of contributors
+//    may be used to endorse or promote products derived from this
+//    software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// -----------------------------------------------------------------------
+// This program is free software; you can redistribute it and/or
+// modify it under the above terms or under the terms of the GNU
+// General Public License as published by the Free Software Foundation;
+// either version 2 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see http://www.gnu.org/licenses/
+// or write to the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA  02110-1301, USA.
+// 
+
+/// A type for representing VHDL vector sizes. This type is equivalent to the `range` of a VHDL vector type,
+/// e.g. *5 downto 3* or *3 to 5*.
+public enum VectorSize: RawRepresentable, Equatable, Hashable, Codable {
+
+    /// The `downto` case. This represents the range as an upper limit down to a lower limit.
+    case downto(upper: Int, lower: Int)
+
+    /// The `to` case. This represents the range as a lower limit to an upper limit.
+    case to(lower: Int, upper: Int)
+
+    /// The raw value is a string.
+    public typealias RawValue = String
+
+    /// The equivalent VHDL code for this type.
+    public var rawValue: String {
+        switch self {
+        case .downto(let upper, let lower):
+            return "\(upper) downto \(lower)"
+        case .to(let lower, let upper):
+            return "\(lower) to \(upper)"
+        }
+    }
+
+    /// Initialse the type from a string. This will return `nil` if the string is not a valid VHDL range.
+    /// - Parameter rawValue: The value to convert.
+    public init?(rawValue: String) {
+        let trimmedValue = rawValue.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard trimmedValue.count > 5, trimmedValue.count < 256 else {
+            return nil
+        }
+        let value = trimmedValue.lowercased()
+        let hasDownto = value.contains(" downto ")
+        let hasTo = value.contains(" to ")
+        guard hasDownto != hasTo else {
+            return nil
+        }
+        guard hasDownto else {
+            let components = value.components(separatedBy: " to ")
+            guard let lhs = Int(components.first ?? ""), let rhs = Int(components.last ?? "") else {
+                return nil
+            }
+            self = .to(lower: lhs, upper: rhs)
+            return
+        }
+        let components = value.components(separatedBy: " downto ")
+        guard let lhs = Int(components.first ?? ""), let rhs = Int(components.last ?? "") else {
+            return nil
+        }
+        self = .downto(upper: lhs, lower: rhs)
+        return
+    }
+
+}
---
+++ Tests/VHDLMachinesTests/codeStructure/VectorSizeTests.swift
@@ -0,0 +1,113 @@
+// VectorSizeTests.swift
+// Machines
+// 
+// Created by Morgan McColl.
+// Copyright © 2023 Morgan McColl. All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+// 
+// 3. All advertising materials mentioning features or use of this
+//    software must display the following acknowledgement:
+// 
+//    This product includes software developed by Morgan McColl.
+// 
+// 4. Neither the name of the author nor the names of contributors
+//    may be used to endorse or promote products derived from this
+//    software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// -----------------------------------------------------------------------
+// This program is free software; you can redistribute it and/or
+// modify it under the above terms or under the terms of the GNU
+// General Public License as published by the Free Software Foundation;
+// either version 2 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see http://www.gnu.org/licenses/
+// or write to the Free Software Foundation, Inc., 51 Franklin Street,
+// Fifth Floor, Boston, MA  02110-1301, USA.
+// 
+
+@testable import VHDLMachines
+import XCTest
+
+/// Test class for ``VectorSize``.
+final class VectorSizeTests: XCTestCase {
+
+    /// Test raw value is created correctly.
+    func testRawValue() {
+        let downto = VectorSize.downto(upper: 12, lower: 5)
+        XCTAssertEqual(downto.rawValue, "12 downto 5")
+        let to = VectorSize.to(lower: 2, upper: 7)
+        XCTAssertEqual(to.rawValue, "2 to 7")
+    }
+
+    /// Test valid raw data is convertible for `downto` case.
+    func testDowntoValidRawValueInit() {
+        let raw = "12 downto 5"
+        let result = VectorSize(rawValue: raw)
+        let expected = VectorSize.downto(upper: 12, lower: 5)
+        XCTAssertEqual(result, expected)
+    }
+
+    /// Test valid raw data is convertible for `to` case.
+    func testToValidRawValueInit() {
+        let raw = "2 to 7"
+        let result = VectorSize(rawValue: raw)
+        let expected = VectorSize.to(lower: 2, upper: 7)
+        XCTAssertEqual(result, expected)
+    }
+
+    /// Test result is nil for data that is too small.
+    func testSmallRawData() {
+        let raw = "2 t 4"
+        XCTAssertNil(VectorSize(rawValue: raw))
+        let raw2 = ""
+        XCTAssertNil(VectorSize(rawValue: raw2))
+    }
+
+    /// Test init returns nil when `downto` and `to` are present in raw value.
+    func testDowntoAndToCase() {
+        let raw = "2 downto to 4"
+        XCTAssertNil(VectorSize(rawValue: raw))
+    }
+
+    /// Test init returns nil for non-numeric `downto` case.
+    func testNonNumericDowntoCase() {
+        let raw = "a downto 4"
+        XCTAssertNil(VectorSize(rawValue: raw))
+    }
+
+    /// Test init returns nil for non-numeric `to` case.
+    func testNonNumericToCase() {
+        let raw = "2 to b"
+        XCTAssertNil(VectorSize(rawValue: raw))
+    }
+
+}
