3a4582288d69931136a522ca511a2de25e69d643
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-02-21 19:51:05 +1000

    Added extra spacing for linter.

--- Sources/VHDLParsing/Variable.swift
+++ Sources/VHDLParsing/Variable.swift
@@ -11,15 +11,15 @@ import Foundation
 public protocol Variable {
 
     /// The type of the variable.
-    var type: SignalType {get set}
+    var type: SignalType { get set }
 
     /// The name of the variable.
-    var name: VariableName {get set}
+    var name: VariableName { get set }
 
     /// The default value of the variable.
-    var defaultValue: Expression? {get set}
+    var defaultValue: Expression? { get set }
 
     /// The range of the variable.
-    var comment: Comment? {get set}
+    var comment: Comment? { get set }
 
 }
