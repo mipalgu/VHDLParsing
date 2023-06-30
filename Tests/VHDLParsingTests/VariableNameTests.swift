8adb73248f084d2d7208c483b83799cf472f4ffd
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-07-01 08:38:15 +1000

    Add true and false to reserved words.

--- Tests/VHDLParsingTests/VariableNameTests.swift
+++ Tests/VHDLParsingTests/VariableNameTests.swift
@@ -115,6 +115,8 @@ final class VariableNameTests: XCTestCase {
         XCTAssertNil(VariableName(rawValue: "xor"))
         XCTAssertNil(VariableName(rawValue: "clk 12"))
         XCTAssertEqual(VariableName(rawValue: "x "), VariableName(text: "x"))
+        XCTAssertNil(VariableName(rawValue: "false"))
+        XCTAssertNil(VariableName(rawValue: "true"))
     }
 
 }
